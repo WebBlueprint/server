0d860eeaa621be2a19a30e7b8ccae24c1110d209
handevmin <handevmin@gmail.com>
2023-11-16 11:05:51 +0900

    인기있는 프로리스트 조회기능 구현

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const searchRoutes = require('./search');
+const popularProsRoutes = require('./popularPros');
 
 router.use('/search', searchRoutes);
+router.use('/popular-pros', popularProsRoutes);
 
 module.exports = router;
