873a4e17758de1270ae7228377cf8b66685817da
handevmin <handevmin@gmail.com>
2024-02-04 22:35:49 +0900

    Updated myDrivingRange API

--- app/src/routes/lesson/myDrivingRange.js
+++ app/src/routes/lesson/myDrivingRange.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { GolfCourse, User } = require('../../models/model');
+const { GolfCourse, User, Pro } = require('../../models/model');
 
 router.get('/:userId', async (req, res) => {
     try {
@@ -16,12 +16,16 @@ router.get('/:userId', async (req, res) => {
             return res.status(404).send('Golf Course not found');
         }
 
+        const pros = await Pro.find({ golf_course_id: golfCourse._id }).select('name email phone');
+
         // 응답 데이터 구성
         const response = {
             googleMap: golfCourse.map_link, // 구글 맵 링크
             golfCourseName: golfCourse.name, // 골프 코스 이름
             address: golfCourse.address, // 주소
-            rating: golfCourse.rating // 별점
+            rating: golfCourse.rating, // 별점
+            pros: pros // 프로 리스트
+
         };
 
         res.json(response);
