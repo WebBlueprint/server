7fecebb03fc5af84a4ef7df05eb2bb70e8db2b91
Seungwoo Yun <yoooonseungwoo@gmail.com>
2024-03-01 21:08:45 +0800

    socket io 수정

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,9 +1,24 @@
 "use strict";
+const dotenv = require("dotenv");
+dotenv.config({ path: '../../.env' });
 
+const { createServer } = require("http")
+// http 프로로콜 통해서 서버를 하나 만듦
 const app = require("../app");
-require('dotenv').config({ path: '.env' });
-const PORT = process.env.PORT || 3000;
+const httpServer = createServer(app)
+const { Server } = require("socket.io")
+// io라고 하는 웹소켓 서버를 만든 것
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3006",
+        credentials: true,
+    }
+})
+// io를 io.js로부터 받아옴. 
+require("../src/utils/io")(io)
+
+
+httpServer.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
